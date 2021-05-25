a4309b5a24f228f39420d03a3fc4df08a9f2c938
Viktory Saveleva <viktorysaveleva@gmail.com>
2021-05-25 23:39:32 +0300

    FirstHomeTask

---
+++ 3task.py
@@ -0,0 +1,11 @@
+n = (input('Введите число: '))
+n = str(n)
+nn = n + n
+#print(nn)
+nnn = n + n + n
+#print(nnn)
+#n = int(n)
+#nn = int(nn)
+#nnn = int(nnn)
+s = int(n) + int(nn) + int(nnn)
+print(s)
